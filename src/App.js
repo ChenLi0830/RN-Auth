e8974a7e5ff7f7d4d06e9d066cfd52a14976f144
Chen Li <yichen.li0830@gmail.com>
2017-01-20 14:55:09 -0800

    Use index to get access to all the common components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import {Header} from './components/common'
 
 class App extends Component {
   render(){
     return <View>
+      <Header title="Authentication"/>
       <Text>
         An App
       </Text>
---
+++ src/components/common/index.js
@@ -0,0 +1,11 @@
+// import Button from './Button';
+// import Card from './Card';
+// import CardSection from './CardSection';
+// import Header from './header';
+//
+// export {Button, Card, CardSection, Header};
+
+export * from './Button';
+export * from './CardSection';
+export * from './Card';
+export * from './Header';

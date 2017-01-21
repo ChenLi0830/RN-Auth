ec963f1676ceccc16cf95207304b9c996032f016
Chen Li <yichen.li0830@gmail.com>
2017-01-20 22:06:05 -0800

    Use reusable Input component and create styles for the input box

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-import {Button, Card, CardSection} from './common';
+import {Button, Card, CardSection, Input} from './common';
 
-class LoginForm extends Component{
-  render(){
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ""};
+  }
+  
+  render() {
     return <Card>
       <CardSection>
+        <Input label="Email"
+               onChangeText={text => this.setState({text})}
+               value={this.state.text}/>
       </CardSection>
       
       <CardSection>
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -9,3 +9,4 @@ export * from './Button';
 export * from './CardSection';
 export * from './Card';
 export * from './Header';
+export * from './Input';
